fa347b2020def80fa945f1f23799be8e0e4bece2
itaispector <spector37@gmail.com>
2015-06-22 14:11:07 +0300

    aa

---
+++ app/src/main/java/com/myvaad/myvaad/MainActivity.java
@@ -0,0 +1,281 @@
+/*
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.myvaad.myvaad;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.parse.Parse;
+import adapters.NavAdapter;
+import dialogs.AboutDialog;
+
+/**
+ * This example illustrates a common usage of the DrawerLayout widget
+ * in the Android support library.
+ * <p/>
+ * <p>When a navigation (left) drawer is present, the host activity should detect presses of
+ * the action bar's Up affordance as a signal to open and close the navigation drawer. The
+ * ActionBarDrawerToggle facilitates this behavior.
+ * Items within the drawer should fall into one of two categories:</p>
+ * <p/>
+ * <ul>
+ * <li><strong>View switches</strong>. A view switch follows the same basic policies as
+ * list or tab navigation in that a view switch does not create navigation history.
+ * This pattern should only be used at the root activity of a task, leaving some form
+ * of Up navigation active for activities further down the navigation hierarchy.</li>
+ * <li><strong>Selective Up</strong>. The drawer allows the user to choose an alternate
+ * parent for Up navigation. This allows a user to jump across an app's navigation
+ * hierarchy at will. The application should treat this as it treats Up navigation from
+ * a different task, replacing the current task stack using TaskStackBuilder or similar.
+ * This is the only form of navigation drawer that should be used outside of the root
+ * activity of a task.</li>
+ * </ul>
+ * <p/>
+ * <p>Right side drawers should be used for actions, not navigation. This follows the pattern
+ * established by the Action Bar that navigation should be to the left and actions to the right.
+ * An action should be an operation performed on the current contents of the window,
+ * for example enabling or disabling a data overlay on top of the current content.</p>
+ */
+public class MainActivity extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private RelativeLayout mDrawerContent,navUserNamePic;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mPlanetTitles;
+    private ImageView userImg,navLogoutBtn;
+    private TextView navUserName;
+    Typeface font;
+    NavAdapter adapter;
+    ParseDB db;
+    Context fa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fa=this;
+        //To connect with parse - we need to provide 2 keys: appId & clientId
+        String appId="QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
+        String clientId="OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
+        //Initialize with keys
+        Parse.initialize(this, appId, clientId);
+        db=ParseDB.getInstance(this);
+
+        mTitle = mDrawerTitle = getTitle();
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerContent = (RelativeLayout) findViewById(R.id.relative_layout);
+        navUserNamePic = (RelativeLayout)findViewById(R.id.nav_username_pic);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        /*
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));*/
+        adapter =  new NavAdapter(this,db.isCurrentUserAdmin());
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //Action bar background
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#007ca2"));
+        getActionBar().setBackgroundDrawable(colorDrawable);
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+                ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
+        userImg=(ImageView)findViewById(R.id.user_nav_img);
+        navUserName=(TextView)findViewById(R.id.navUserName);
+        userImg.setImageBitmap(db.getcurrentUserPicture());
+        String navUserNameString=db.getcurrentUserFamilyName();
+        font = Typeface.createFromAsset(getAssets(), "fonts/GISHA.TTF");
+        navUserName.setTypeface(font);
+        navUserName.setText(navUserNameString);
+
+        navLogoutBtn=(ImageView)findViewById(R.id.navLogoutBtn);
+        navLogoutBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        navLogoutBtn.setImageResource(R.drawable.nav_logout_btn_down);
+                        break;
+                    }
+                    case MotionEvent.ACTION_CANCEL:{
+                        navLogoutBtn.setImageResource(R.drawable.nav_logout_btn);
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+        navLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.logOutUser(MainActivity.this);
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    // Called whenever we call invalidateOptionsMenu()
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerContent);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        switch (item.getItemId()){
+            case R.id.about:
+                AboutDialog dialog=new AboutDialog(this);
+                dialog.showAboutDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment0 = new NoticeBoardScreen();
+        Fragment fragment1 = new FailuresScreen();
+        Fragment fragment3 = new PaymentsScreen();
+        Fragment fragment4 = new UsersScreen();
+        FragmentManager fragmentManager = getFragmentManager();
+        switch (position){
+            case 0:
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment0).commit();
+                break;
+            case 1:
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment1).commit();
+                break;
+            case 2:
+                Toast.makeText(this,"2",Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+            	fragmentManager.beginTransaction().replace(R.id.content_frame, fragment3).commit();
+                break;
+            case 4:
+            	fragmentManager.beginTransaction().replace(R.id.content_frame, fragment4).commit();
+                break;
+        }
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        adapter.selected(position);
+        mDrawerLayout.closeDrawer(mDrawerContent);
+    }
+    public void userProfile(View v){
+        Intent i = new Intent(this,UserProfile.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.anim_slide_in_left,R.anim.anim_slide_out_left);
+        mDrawerLayout.closeDrawer(mDrawerContent);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
