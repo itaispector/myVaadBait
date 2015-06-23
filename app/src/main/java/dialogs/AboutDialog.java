ffde407af94847dbf15702cb1e2c632643708ac0
Daniel Mashraki <danielm1199@gmail.com>
2015-06-23 17:44:44 +0300

    סידור באגים

--- app/src/main/java/dialogs/AboutDialog.java
+++ app/src/main/java/dialogs/AboutDialog.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.view.View;
-
 import com.myvaad.myvaad.R;
 
 public class AboutDialog {
