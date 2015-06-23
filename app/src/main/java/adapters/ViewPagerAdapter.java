a7e888a255242608e08e7b087b62cbe023c57b42
itaispector <spector37@gmail.com>
2015-06-23 14:54:26 +0300

    code structure fixed

--- app/src/main/java/adapters/ViewPagerAdapter.java
+++ app/src/main/java/adapters/ViewPagerAdapter.java
@@ -10,47 +10,45 @@ import com.myvaad.myvaad.UserExpenses;
 
 
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
- 
+
     CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
     int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
- 
- 
+
+
     // Build a Constructor and assign the passed Values to appropriate values in the class
-    public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+    public ViewPagerAdapter(FragmentManager fm, CharSequence mTitles[], int mNumbOfTabsumb) {
         super(fm);
- 
+
         this.Titles = mTitles;
         this.NumbOfTabs = mNumbOfTabsumb;
- 
+
     }
- 
+
     //This method return the fragment for the every position in the View Pager
     @Override
     public Fragment getItem(int position) {
- 
-        if(position == 0) // if the position is 0 we are returning the First tab
+
+        if (position == 0) // if the position is 0 we are returning the First tab
         {
-        	UserExpenses tab1 = new UserExpenses();
+            UserExpenses tab1 = new UserExpenses();
             return tab1;
-        }
-        else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
-        {
-        	BuildingExpenses tab2 = new BuildingExpenses();
+        } else {
+            BuildingExpenses tab2 = new BuildingExpenses();
             return tab2;
         }
- 
- 
+
+
     }
- 
+
     // This method return the titles for the Tabs in the Tab Strip
- 
+
     @Override
     public CharSequence getPageTitle(int position) {
         return Titles[position];
     }
- 
+
     // This method return the Number of tabs for the tabs Strip
- 
+
     @Override
     public int getCount() {
         return NumbOfTabs;
