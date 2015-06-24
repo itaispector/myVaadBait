b3017229391c048590298c6eb3d4f376113b377c
itaispector <spector37@gmail.com>
2015-06-24 14:31:48 +0300

    changesssss

--- app/src/main/java/com/myvaad/myvaad/NotInBuilding.java
+++ app/src/main/java/com/myvaad/myvaad/NotInBuilding.java
@@ -30,38 +30,38 @@ public class NotInBuilding extends FragmentActivity {
     EditText bCodeNumber;
     View dialogLayout;
     Dialog joinBuildingDialog;
-    Button ok, cancel, createBuildingBtn, joinBuildingBtn;
-    ImageView logout;
+    Button ok, cancel, createBuildingBtn, joinBuildingBtn,logout;
     public static Activity closeNotInBuildingActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getActionBar().hide();
         setContentView(R.layout.not_in_building);
-        String appId="QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
-        String clientId="OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
+        String appId = "QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
+        String clientId = "OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
         //Initialize with keys
         Parse.initialize(this, appId, clientId);
-        db=ParseDB.getInstance(this);
-        closeNotInBuildingActivity=this;
+        db = ParseDB.getInstance(this);
+        closeNotInBuildingActivity = this;
 
-        createBuildingBtn=(Button)findViewById(R.id.CreateBuildingBtn);
-        createBuildingBtn.setOnClickListener(new View.OnClickListener(){
+        createBuildingBtn = (Button) findViewById(R.id.CreateBuildingBtn);
+        createBuildingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createBuilding();
             }
         });
 
-        joinBuildingBtn=(Button)findViewById(R.id.JoinBuildingBtn);
-        joinBuildingBtn.setOnClickListener(new View.OnClickListener(){
+        joinBuildingBtn = (Button) findViewById(R.id.JoinBuildingBtn);
+        joinBuildingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 joinBuilding();
             }
         });
 
-        logout=(ImageView)findViewById(R.id.NotInBuildingLogout);
+        logout = (Button) findViewById(R.id.NotInBuildingLogout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,77 +71,85 @@ public class NotInBuilding extends FragmentActivity {
 
 
     }
-    public void joinBuilding(){
-            dialogLayout = View.inflate(this, R.layout.join_building_dialog, null);
-            joinBuildingDialog = new Dialog(this);
-            joinBuildingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            joinBuildingDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-            joinBuildingDialog.setContentView(dialogLayout);
-            joinBuildingDialog.show();
 
-            ok = (Button)dialogLayout.findViewById(R.id.joinBuildingDialogOkBtn);
-            cancel = (Button)dialogLayout.findViewById(R.id.joinBuildingDialogCancelBtn);
-            bCodeNumber=(EditText) dialogLayout.findViewById(R.id.JoinBuildingEnterBuildingCode);
+    public void joinBuilding() {
+        dialogLayout = View.inflate(this, R.layout.join_building_dialog, null);
+        joinBuildingDialog = new Dialog(this);
+        joinBuildingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        joinBuildingDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        joinBuildingDialog.setContentView(dialogLayout);
+        joinBuildingDialog.show();
 
-            ok.setOnClickListener(new View.OnClickListener() {
+        ok = (Button) dialogLayout.findViewById(R.id.joinBuildingDialogOkBtn);
+        cancel = (Button) dialogLayout.findViewById(R.id.joinBuildingDialogCancelBtn);
+        bCodeNumber = (EditText) dialogLayout.findViewById(R.id.JoinBuildingEnterBuildingCode);
 
-                @Override
-                public void onClick(View v) {
-                    dialogButtons(v);
+        ok.setOnClickListener(new View.OnClickListener() {
 
-                }
-            });
+            @Override
+            public void onClick(View v) {
+                dialogButtons(v);
 
-            cancel.setOnClickListener(new View.OnClickListener() {
+            }
+        });
 
-                @Override
-                public void onClick(View v) {
-                    dialogButtons(v);
+        cancel.setOnClickListener(new View.OnClickListener() {
 
-                }
-            });
+            @Override
+            public void onClick(View v) {
+                dialogButtons(v);
+
+            }
+        });
 
     }
 
     public void dialogButtons(View v) {
-        String buildingCodeText=bCodeNumber.getText().toString();
-        if (v.getId() == R.id.joinBuildingDialogCancelBtn){
+        String buildingCodeText = bCodeNumber.getText().toString();
+        if (v.getId() == R.id.joinBuildingDialogCancelBtn) {
             //Exit from keyboard when the dialog dismissed
-            InputMethodManager imm=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
             joinBuildingDialog.dismiss();
         } else {
-            if(buildingCodeText.length()<6){
-                Toast toast=Toast.makeText(this,getString(R.string.b_code_less_nums),Toast.LENGTH_LONG);
+            if (buildingCodeText.length() < 6) {
+                Toast toast = Toast.makeText(this, getString(R.string.b_code_less_nums), Toast.LENGTH_LONG);
                 toast.setGravity(Gravity.CENTER, 0, 0);
                 toast.show();
                 //Exit from keyboard when the dialog dismissed
-                InputMethodManager imm=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
-            }else{
-                if(db.isBuildingCodeExists(buildingCodeText)){
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            } else {
+                if (db.isBuildingCodeExists(buildingCodeText)) {
                     db.updateUserBuildingCode(buildingCodeText);
                     //Exit from keyboard when the dialog dismissed
-                    InputMethodManager imm=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                     Intent i = new Intent(this, MainActivity.class);
                     startActivity(i);
                     this.finish();
-                }else{
-                    Toast toast=Toast.makeText(this,R.string.problem_text,Toast.LENGTH_LONG);
+                } else {
+                    Toast toast = Toast.makeText(this, R.string.problem_text, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
                     //Exit from keyboard when the dialog dismissed
-                    InputMethodManager imm=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                 }
             }
         }
     }
-    public void createBuilding(){
+
+    public void createBuilding() {
         Fragment mFragment = new CreateBuilding();
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_framee, mFragment).commit();
+        fragmentManager.beginTransaction().replace(R.id.content_framee, mFragment).addToBackStack("NotInBuilding").commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.popBackStack();
     }
 }
 
