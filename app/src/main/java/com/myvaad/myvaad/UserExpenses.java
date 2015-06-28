647089d517cc07921681bdf264cca1b1b7f82007
shlomifresko <shlomi.fresko@gmail.com>
2015-06-28 18:11:06 +0300

    טעינת  עמוד ברקע

--- app/src/main/java/com/myvaad/myvaad/UserExpenses.java
+++ app/src/main/java/com/myvaad/myvaad/UserExpenses.java
@@ -1,90 +1,80 @@
 package com.myvaad.myvaad;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQueryAdapter;
 import com.parse.Parse;
 import com.parse.ParseQuery;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
+
+import adapters.BuildingExpensesAdapter;
+import adapters.UserExpensesAdapter;
+
 public class UserExpenses extends Fragment {
-	ParseDB db;
-	int usersInBuilding = 5;
-	
-	  @Override
-	   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	       View rootView =inflater.inflate(R.layout.user_expenses_layout,container,false);
-	        String appId="QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
-	        String clientId="OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
-	        //Initialize with keys
-	        Parse.initialize(getActivity(), appId, clientId);
-	    	db=ParseDB.getInstance(getActivity());
-	    	
-	    	//simple query
-	        //ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(getActivity(), "noticeBoard");
-	    	
-	    	//adapter with defined query
-	    	ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(getActivity(), new ParseQueryAdapter.QueryFactory<ParseObject>() {
+    private ParseDB db;
+    private TextView buildingTotalExpenses;
+    int totalExpensesAmount = 0;
+    private ListView listView;
+    private UserExpensesAdapter customParseAdapter;
 
-				@Override
-				public ParseQuery<ParseObject> create() {
-					//Define query
-					ParseQuery query = new ParseQuery("payments");
-					query.whereEqualTo("buildingCode", "239250");
-					query.whereEqualTo("paymentType", "vaad");
-					query.whereEqualTo("paymentApproved", true);
-					query.whereEqualTo("paidBy", db.getCurrentUserObjectId());
-					query.orderByDescending("createdAt");
-					return query;
-				}
-			}){
-	    		@Override
-	    		public View getItemView(ParseObject object, View v, ViewGroup parent) {
-	    		  if (v == null) {
-	    		    v = View.inflate(getContext(), R.layout.expenses_adapter_item, null);
-	    		  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.user_expenses_layout, container, false);
 
-	    		  // Take advantage of ParseQueryAdapter's getItemView logic for
-	    		  // populating the main TextView/ImageView.
-	    		  // The IDs in your custom layout must match what ParseQueryAdapter expects
-	    		  // if it will be populating a TextView or ImageView for you.
-	    		  //super.getItemView(object, v, parent);
+        //Initialize parse
+        //Parse.initialize(getActivity());
+        db = ParseDB.getInstance(getActivity());
 
-	    		  // Do additional configuration before returning the View.
-	    		  TextView descriptionView = (TextView) v.findViewById(R.id.expenseDescription);
-	    		  descriptionView.setText(object.getString("description"));
-	    		  TextView amountView = (TextView)v.findViewById(R.id.expenseAmount);
-	    		  String thisExpense = object.getString("amount");
-	    		  amountView.setText(getActivity().getString(R.string.shekel)+thisExpense);
-	    		  
-	    		  
-	    		  TextView createTimeView = (TextView)v.findViewById(R.id.expenseCreatTime);
-	    		  String time = object.getCreatedAt().toLocaleString();
-	    		  createTimeView.setText(time);
-	    		  
-	    		  
-	    		  return v;
-	    		  
-	    		  
-	    		}
-	    	};
+        buildingTotalExpenses = (TextView) rootView.findViewById(R.id.userTotalExpensesAmount);
+        buildingTotalExpenses.setText("");
 
-	    	//adapter.setTextKey("content");
-	    	
-	        //adapter.setImageKey("userPic");
+        // Initialize the subclass of ParseQueryAdapter
+        customParseAdapter = new UserExpensesAdapter(getActivity(), db.getCurrentUserObjectId());
+        listView = (ListView) rootView.findViewById(R.id.userExpensesListview);
+        listView.setAdapter(customParseAdapter);
+        customParseAdapter.loadObjects();
 
-	        ListView listView = (ListView) rootView.findViewById(R.id.userExpensesListview);
-	        listView.setAdapter(adapter);
-	        
-	        TextView buildingTotalExpenses = (TextView)rootView.findViewById(R.id.userTotalExpensesAmount);
-	    	buildingTotalExpenses.setText( getActivity().getString(R.string.total)+" "+ getActivity().getString(R.string.shekel)+ db.getCurrentUserTotalExpenses());
-	    	
-	        return rootView;
-	   }
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("payments");
+        //Query Constraints-->all users from specific building
+        query.whereEqualTo("buildingCode", db.getCurrentUserBuildingCode());
+        query.whereEqualTo("paymentType", "vaad");
+        query.whereEqualTo("paidBy", db.getCurrentUserObjectId());
+        query.whereEqualTo("paymentApproved", true);
+        query.orderByDescending("createdAt");
+
+        //finding all payments for current user
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> payments, ParseException e) {
+                if (e == null) {
+                    for (ParseObject paymentRow : payments) {
+
+                        //get specific data from each row
+                        String amount = paymentRow.getString("amount");
+                        totalExpensesAmount += Integer.parseInt(amount);
+
+                    }
+
+                    buildingTotalExpenses.setText(getActivity().getString(R.string.total) + " " + getActivity().getString(R.string.shekel) + totalExpensesAmount);
+
+                } else {//ParseException
+                    Log.e("***Parse Exception***", e.getLocalizedMessage());
+                }
+            }
+        });
+
+        return rootView;
+    }
 
 }
