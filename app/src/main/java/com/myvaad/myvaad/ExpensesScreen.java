67ccc375b234308706479b1e3b4e72d43e6b10bd
shlomifresko <shlomi.fresko@gmail.com>
2015-06-28 18:06:30 +0300

    טעינת  עמוד ברקע

--- app/src/main/java/com/myvaad/myvaad/ExpensesScreen.java
+++ app/src/main/java/com/myvaad/myvaad/ExpensesScreen.java
@@ -15,54 +15,51 @@ import android.view.*;
 import adapters.ViewPagerAdapter;
 
 
-public class ExpensesScreen extends Fragment{
-	ParseDB db;
+public class ExpensesScreen extends Fragment {
+    ParseDB db;
     ViewPager pager;
     ViewPagerAdapter adapter;
     SlidingTabLayout tabs;
 
-    int Numboftabs =2;
+    int Numboftabs = 2;
     FragmentActivity myContext;
-    
+
     FragmentManager fragManager;
-    
-   
-    
-    
+
+
     @Override
     public void onAttach(Activity activity) {
-        myContext=(FragmentActivity) activity;
+        myContext = (FragmentActivity) activity;
         super.onAttach(activity);
     }
-   
-    
-    
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-        CharSequence Titles[]={getActivity().getString(R.string.myExpenses),getActivity().getString(R.string.buildingExpenses)};
 
-    	String appId="QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
-        String clientId="OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        CharSequence Titles[] = {getActivity().getString(R.string.myExpenses), getActivity().getString(R.string.buildingExpenses)};
+
+        String appId = "QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
+        String clientId = "OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
         //Initialize with keys
         Parse.initialize(getActivity(), appId, clientId);
-    	db=ParseDB.getInstance(getActivity());
-    	//final View rootView = inflater.inflate(resource, root, attachToRoot)
-    	final View rootView = inflater.inflate(R.layout.expenses_main_layout, container, false);
-    	 
-    	
-    	fragManager = myContext.getSupportFragmentManager();
+        db = ParseDB.getInstance(getActivity());
+        //final View rootView = inflater.inflate(resource, root, attachToRoot)
+        final View rootView = inflater.inflate(R.layout.expenses_main_layout, container, false);
+
+
+        fragManager = myContext.getSupportFragmentManager();
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
-        adapter =  new ViewPagerAdapter(fragManager,Titles,Numboftabs);
-        
+        adapter = new ViewPagerAdapter(fragManager, Titles, Numboftabs);
+
         // Assigning ViewPager View and setting the adapter
         pager = (ViewPager) rootView.findViewById(R.id.pager);
         pager.setAdapter(adapter);
- 
+
         // Assiging the Sliding Tab Layout View
         tabs = (SlidingTabLayout) rootView.findViewById(R.id.tabs);
         tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
- 
+
         // Setting Custom Color for the Scroll bar indicator of the Tab View
         tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
@@ -70,11 +67,11 @@ public class ExpensesScreen extends Fragment{
                 return getResources().getColor(R.color.tabsScrollColor);
             }
         });
- 
+
         // Setting the ViewPager For the SlidingTabsLayout
         tabs.setViewPager(pager);
-    	
-    	return rootView;
-	}
+
+        return rootView;
+    }
 
 }
