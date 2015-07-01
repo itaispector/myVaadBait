e25130f0d580c4b1295726b8daaa1cb649943de0
shlomifresko <shlomi.fresko@gmail.com>
2015-07-01 15:39:45 +0300

    מחיקת איתחול פארס

--- app/src/main/java/com/myvaad/myvaad/FailuresScreen.java
+++ app/src/main/java/com/myvaad/myvaad/FailuresScreen.java
@@ -61,10 +61,7 @@ public class FailuresScreen extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        String appId = "QdwF666zm76ORQcn4KF6JNwDfsb6cj97QunbpT1s";
-        String clientId = "OiJI3KdONEN9jML6Mi6r6iQTpR8mIOBv3YgsUhdv";
-        //Initialize with keys
-        Parse.initialize(getActivity(), appId, clientId);
+
         db = ParseDB.getInstance(getActivity());
         View rootView = inflater.inflate(R.layout.failures_screen, container, false);
         //show loader
