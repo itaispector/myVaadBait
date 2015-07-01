dffdcd774b75428167d6ff2b45734fc8831ae725
Daniel Mashraki <danielm1199@gmail.com>
2015-07-01 21:31:34 +0300

    פרופיל משתמש

--- app/src/main/java/com/myvaad/myvaad/ParseDB.java
+++ app/src/main/java/com/myvaad/myvaad/ParseDB.java
@@ -516,7 +516,40 @@ public class ParseDB {
         currentUser.saveInBackground();
     }
 
-    //This method sign up new building in class(table)buildings-->created by building Admin
+
+    /**
+     * *******************UPDATED BY DANIEL NEWWWWW******************************
+     */
+    //This method update familyname to currentUser--
+    protected void updateUserEmail(String email) {
+        ParseUser currentUser = getcurrentUser();
+        currentUser.put("email", email);
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                int errorCode = e.getCode();
+                switch (errorCode) {
+                    case 203:
+                        toast = Toast.makeText(context, context.getString(R.string.emailTaken), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 150);
+                        toast.show();
+                        break;
+                    case 125:
+                        toast = Toast.makeText(context, context.getString(R.string.InvalidEmail), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 150);
+                        toast.show();
+                        break;
+                    default:
+                        Log.e("**Exception CODE**", errorCode + "");
+                        Log.e("*****Exception*****", e.getLocalizedMessage());
+                        break;
+                }
+            }
+        });
+    }
+
+                //This method sign up new building in class(table)buildings-->created by building Admin
+
     protected void signUpBuilding(String buildingCode, String address, String paypalEmail, String numberOfHouses) {
         //The user that sign Up the Building is the admin
         ParseUser currentUser = getcurrentUser();
@@ -1680,19 +1713,32 @@ protected List getCurrentUserFailuresBoard() {
     }
 
 
-    /********************************** NEW BY DANIEL *********************************/
-    protected void updateUserFamilyNameInNotices(ParseUser user) {
+    /**
+     * ******************************* NEW BY DANIEL ********************************
+     */
+    protected void updateUserFamilyNameInNotices(ParseUser user, final String familyName) {
         String cBuilding = getCurrentUserBuildingCode();
         ParseQuery<ParseObject> query = ParseQuery.getQuery("noticeBoard");
         query.whereEqualTo("user", user);
-        try {
-          // ParseObject notice =
-                   query.find();
+        final List daniel= new ArrayList();
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                for(ParseObject notice : list){
+                    notice.put("userFamilyName",familyName);
+                    daniel.add(notice);
+                }
+                ParseObject.saveAllInBackground(daniel, new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e!=null){
+                            Toast.makeText(context,"קרתה תקלה! אנא נסה/י שנית",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
 
-        } catch (ParseException e) {
-            Log.i("***Parse Exception****", e.getLocalizedMessage());
-            e.printStackTrace();
-        }
     }
 
 }
