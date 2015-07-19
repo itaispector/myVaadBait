8913eab908a6f38b656f5f444982ab0eeffcb219
itaispector <spector37@gmail.com>
2015-07-19 18:02:30 +0300

    updates

--- app/src/main/java/adapters/PaymentsAdminVaadBaitAdapter.java
+++ app/src/main/java/adapters/PaymentsAdminVaadBaitAdapter.java
@@ -4,12 +4,14 @@ import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 import com.myvaad.myvaad.PaymentsScreen;
 import com.myvaad.myvaad.R;
 import com.parse.ParseObject;
 
 import adapters.PaymentsUserVaadBaitAdapter.ViewHolder;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,60 +27,74 @@ import android.widget.Toast;
 
 public class PaymentsAdminVaadBaitAdapter extends BaseAdapter {
 
-	private Context context;
-	ViewHolder holder;
-	List<ParseObject> users;
-	
-	public PaymentsAdminVaadBaitAdapter(Context context, List users){
-		this.context=context;
-		this.users=users;
-	}
-	
-	@Override
-	public int getCount() {
-		return users.size();
-	}
+    private Context context;
+    ViewHolder holder;
+    List<ParseObject> users;
+    List paidAll;
 
-	@Override
-	public ParseObject getItem(int idx) {
-		return users.get(idx);
-	}
+    public PaymentsAdminVaadBaitAdapter(Context context, List users, List paidAll) {
+        this.context = context;
+        this.users = users;
+        this.paidAll = paidAll;
+    }
 
-	@Override
-	public long getItemId(int arg0) {
-		return 0;
-	}
-	
-	public void refresh(){
-		notifyDataSetChanged();
-	}
+    @Override
+    public int getCount() {
+        return users.size();
+    }
 
-	public class ViewHolder{
-		TextView name, paidAllTxt;
-	}
-	
-	@Override
-	public View getView(int idx, View convertView, ViewGroup parent) {
-		LayoutInflater myInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
-		if (convertView == null){
+    @Override
+    public ParseObject getItem(int idx) {
+        return users.get(idx);
+    }
+
+    public boolean getPaid(int idx) {
+        boolean state = false;
+        ParseObject user = getItem(idx);
+        String userObjectId = user.getObjectId();
+        if (paidAll != null) {
+            state = paidAll.contains(userObjectId);
+        }
+        return state;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        return 0;
+    }
+
+    public class ViewHolder {
+        TextView name, paidAllTxt;
+    }
+
+    @Override
+    public View getView(int idx, View convertView, ViewGroup parent) {
+        LayoutInflater myInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
             //inflating row view from layout xml
-			convertView = myInflater.inflate(R.layout.payments_vaad_bait_admin_list_view_row, null);           
-            holder = new ViewHolder();     
-            holder.name=(TextView)convertView.findViewById(R.id.paymentsVaadBaitAdminListViewName);
-            holder.paidAllTxt=(TextView)convertView.findViewById(R.id.paymentsVaadBaitAdminListViewPaidAll);
-            
-            convertView.setTag(holder);
-       
-		} else{
-            holder = (ViewHolder)convertView.getTag();		
-		}
-			//setting the data of the row
-			ParseObject user = getItem(idx);
-			String famName = user.getString("familyName");
-			holder.name.setText(context.getResources().getString(R.string.family) + " " + famName);
+            convertView = myInflater.inflate(R.layout.payments_vaad_bait_admin_list_view_row, null);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.paymentsVaadBaitAdminListViewName);
+            holder.paidAllTxt = (TextView) convertView.findViewById(R.id.paymentsVaadBaitAdminListViewPaidAll);
 
-		return convertView;
-	}
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        //setting the data of the row
+        ParseObject user = getItem(idx);
+        String famName = user.getString("familyName");
+        holder.name.setText(context.getResources().getString(R.string.family) + " " + famName);
+        String userObjectId = user.getObjectId();
+        int visibility = View.INVISIBLE;
+        if (paidAll != null) {
+            visibility = (paidAll.contains(userObjectId) ? View.VISIBLE : View.INVISIBLE);
+        }
+        holder.paidAllTxt.setVisibility(visibility);
+
+        return convertView;
+    }
 
 }
