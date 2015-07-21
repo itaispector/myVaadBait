ef4032111128773688e1ec00c995cb9b287730ce
itaispector <spector37@gmail.com>
2015-07-21 18:52:21 +0300

    updates

--- app/src/main/java/com/myvaad/myvaad/NotInBuilding.java
+++ app/src/main/java/com/myvaad/myvaad/NotInBuilding.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,7 +36,7 @@ public class NotInBuilding extends Activity {
     private Toolbar mToolBar;
     View dialogLayout;
     Dialog joinBuildingDialog;
-    Button ok, cancel, createBuildingBtn, joinBuildingBtn,logout;
+    Button ok, cancel, createBuildingBtn, joinBuildingBtn, logout;
     public static Activity closeNotInBuildingActivity;
 
     @Override
@@ -76,73 +78,75 @@ public class NotInBuilding extends Activity {
     }
 
     public void joinBuilding() {
-        dialogLayout = View.inflate(this, R.layout.join_building_dialog, null);
-        joinBuildingDialog = new Dialog(this);
-        joinBuildingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        joinBuildingDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-        joinBuildingDialog.setContentView(dialogLayout);
-        joinBuildingDialog.show();
+        mDialog(R.layout.join_building_dialog);
 
         ok = (Button) dialogLayout.findViewById(R.id.joinBuildingDialogOkBtn);
-        cancel = (Button) dialogLayout.findViewById(R.id.joinBuildingDialogCancelBtn);
+
         bCodeNumber = (EditText) dialogLayout.findViewById(R.id.JoinBuildingEnterBuildingCode);
+        bCodeNumber.addTextChangedListener(textWatcherListener);
         apartmentNumber = (EditText) dialogLayout.findViewById(R.id.add_apartment_number);
+        apartmentNumber.addTextChangedListener(textWatcherListener);
 
         ok.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 dialogButtons(v);
-
-            }
-        });
-
-        cancel.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                dialogButtons(v);
-
             }
         });
 
     }
 
+    //listener to watch if fields are empty or not, if empty add button is disabled
+    private TextWatcher textWatcherListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            boolean check = false;
+            check = (bCodeNumber.getText().toString().isEmpty() || apartmentNumber.getText().toString().isEmpty());
+            ok.setEnabled(!check);
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
     public void dialogButtons(View v) {
         String buildingCodeText = bCodeNumber.getText().toString();
         String userApartmentNumber = apartmentNumber.getText().toString();
-        if (v.getId() == R.id.joinBuildingDialogCancelBtn) {
-            //Exit from keyboard when the dialog dismissed
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-            joinBuildingDialog.dismiss();
+
+        if (buildingCodeText.length() < 6) {
+            Toast toast = Toast.makeText(this, getString(R.string.b_code_less_nums), Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
         } else {
-            if (buildingCodeText.length() < 6) {
-                Toast toast = Toast.makeText(this, getString(R.string.b_code_less_nums), Toast.LENGTH_LONG);
+            if (db.isBuildingCodeExists(buildingCodeText)) {
+                db.updateUserBuildingCode(buildingCodeText, userApartmentNumber);
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                this.finish();
+            } else {
+                Toast toast = Toast.makeText(this, R.string.problem_text, Toast.LENGTH_LONG);
                 toast.setGravity(Gravity.CENTER, 0, 0);
                 toast.show();
-                //Exit from keyboard when the dialog dismissed
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-            } else {
-                if (db.isBuildingCodeExists(buildingCodeText)) {
-                    db.updateUserBuildingCode(buildingCodeText, userApartmentNumber);
-                    //Exit from keyboard when the dialog dismissed
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                    Intent i = new Intent(this, MainActivity.class);
-                    startActivity(i);
-                    this.finish();
-                } else {
-                    Toast toast = Toast.makeText(this, R.string.problem_text, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.CENTER, 0, 0);
-                    toast.show();
-                    //Exit from keyboard when the dialog dismissed
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(bCodeNumber.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                }
             }
         }
+
+    }
+
+    public void mDialog(int layout) {
+        dialogLayout = View.inflate(this, layout, null);
+        joinBuildingDialog = new Dialog(this);
+        joinBuildingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        joinBuildingDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        joinBuildingDialog.setContentView(dialogLayout);
+        joinBuildingDialog.show();
     }
 
     public void createBuilding() {
